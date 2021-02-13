54fa141b20effa729cc52ec20d1e39b34e3c935e
Aditya-011 <adihgr18@gmail.com>
2021-02-13 22:46:45 +0530

    CSS done

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const List = (props) => {
-    return (
-        <>
-              
-              <i className='fa fa-times' onClick={()=>
-                  {
-                      props.onSelect(props.id)
-                  }
-              } aria-hidden='true'></i>
-             <li>{props.text}</li>
-              
-             </>
-    )
-}
+  return (
+    <>
+      {/*<i
+        className="fa fa-times"
+        onClick={() => {
+          props.onSelect(props.id);
+        }}
+        aria-hidden="true"
+    ></i>*/}
+      <li>
+        <span>
+          <input type="checkbox" id="tick" className="tick"></input>
+          <label for="tick">
+            <b className="mainText">{props.text}</b>
+          </label>
+        </span>
+        <span className="time">
+          <span>11:14 PM</span>{" "}
+          <i
+            className="fa fa-trash"
+            onClick={() => {
+              props.onSelect(props.id);
+            }}
+            aria-hidden="true"
+          ></i>
+        </span>
+      </li>
+    </>
+  );
+};
 export default List;
